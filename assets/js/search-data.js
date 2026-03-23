76b9ec3a5d2257e01e5a815a815f3f80cf860e30
gavelino <gavelino@gmail.com>
2026-03-23 13:41:54 +0000

    Deploying to gh-pages from @ gavelino/gavelino.github.io@6d9a79e154a62e2f24af9eeff763dd8fa6b10634 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -500,13 +500,6 @@ ninja.data = [{
           window.open("https://www.dc.ufpi.br", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
